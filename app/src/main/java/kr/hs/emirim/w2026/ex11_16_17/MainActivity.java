915899d22b365716d92d6b0967b21bd637f2eac0
Winni_Lina <9811011sa01@gmail.com>
2021-07-27 13:38:46 +0900

    Edit MainActivity.java(Event Programmnig) Commit

--- app/src/main/java/kr/hs/emirim/w2026/ex11_16_17/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2026/ex11_16_17/MainActivity.java
@@ -3,12 +3,36 @@ package kr.hs.emirim.w2026.ex11_16_17;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] itemArrs = {"영화1", "영화2", "영화3", "영화4", "영화5", "영화6", "영화7", "영화8", "영화9", "영화10"};
+    int[] posterIds = {R.drawable.movie1, R.drawable.movie2, R.drawable.movie3, R.drawable.movie4, R.drawable.movie5, R.drawable.movie6, R.drawable.movie7, R.drawable.movie8, R.drawable.movie9, R.drawable.movie10};
+    ImageView imgv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("스피너와 영화포스터");
+
+        imgv = findViewById(R.id.imgv);
+        Spinner spinner1 = findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,itemArrs);
+        spinner1.setAdapter(adapter);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                imgv.setImageResource(posterIds[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
